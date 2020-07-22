851ebea37c1041e3dfac0a78aefeff4c2ae0a161
Nicolas <nicolas-alvarez117@hotmail.com>
2020-07-22 11:25:10 -0300

    docs

---
+++ docs/precache-manifest.97440625aa63a6efbbcff8708137df84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38de537e630c35b0843f204b30bad7a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e2337bebcb9cfc29bf1",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2337bebcb9cfc29bf1",
+    "url": "/static/js/main.ed6cdb37.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
